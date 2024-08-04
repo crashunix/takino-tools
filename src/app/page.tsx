d449b5ef9a057a987a6c10e4e30f1a75d324f816
Juan Gomes Macario <jgomesmacario@outlook.com>
2024-08-04 00:08:23 -0300

    remove rg generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,11 +22,6 @@ export default function Home() {
           name: "CPF generator",
           description: "Generate a new random CPF",
           path: "/cpf-generator"
-        },
-        {
-          name: "RG generator",
-          description: "Generate a new random RG",
-          path: "/rg-generator"
         }
       ]
     },
